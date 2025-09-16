039787bab13774a4d35b37c8b80713155dbb7728
Ramir-C <163381802+Ramir-C@users.noreply.github.com>
2025-09-16 10:32:05 -0600

    Update db.js

--- Recordando figuras final/server/db.js
+++ Recordando figuras final/server/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'mysql.railway.internal',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'LNNExntdHlbDSNuSvrlayPcrAwBrOQmw',
+  password: process.env.DB_PASSWORD || 'LjwhvlsLCFeIJOEoJzFgvIfyIbqRFWPg',
   database: process.env.DB_NAME || 'railway',
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
